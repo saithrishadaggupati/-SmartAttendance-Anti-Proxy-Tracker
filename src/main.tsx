394a78ec61f176e83324689c9b5feb7404a0fcf5
Sai Thrisha <saitrishadaggupati@gmail.com>
2026-06-03 12:54:26 +0530

    Style Fix: Hardcoded Tailwind v4 import directive rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// @ts-ignore
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
